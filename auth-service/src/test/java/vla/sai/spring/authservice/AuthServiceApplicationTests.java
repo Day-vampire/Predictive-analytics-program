6db666ca6d217349176f9972099ae5c0f6f0f48b
VlaSai <ddddddddebat@gmail.com>
2025-03-11 17:40:41 +0300

    add auth-service

---
+++ auth-service/src/test/java/vla/sai/spring/authservice/AuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package vla.sai.spring.authservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
