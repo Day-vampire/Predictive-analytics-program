dd3f2ae349406e9504f26271e076bd8c3e5f74bf
VlaSai <ddddddddebat@gmail.com>
2025-03-14 11:51:13 +0300

    skillet of file service

---
+++ file-service/src/test/java/vla/sai/spring/fileservice/FileServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package vla.sai.spring.fileservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
