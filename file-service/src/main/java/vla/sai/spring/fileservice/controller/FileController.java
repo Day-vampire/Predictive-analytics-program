a7f2f39e9074de5edd0c6d3e882849c79e203fe3
VlaSai <ddddddddebat@gmail.com>
2025-04-01 12:37:38 +0300

    added fileUtil2

--- file-service/src/main/java/vla/sai/spring/fileservice/controller/FileController.java
+++ file-service/src/main/java/vla/sai/spring/fileservice/controller/FileController.java
@@ -1,111 +1,92 @@
 package vla.sai.spring.fileservice.controller;
 
+import java.io.File;
 import java.io.IOException;
 import java.lang.reflect.Modifier;
 import java.lang.reflect.Field;
-import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-import vla.sai.spring.fileservice.entity.FileInfo;
 import vla.sai.spring.fileservice.entity.FileInfo_;
 import vla.sai.spring.fileservice.exeption.StorageFileNotFoundException;
 import vla.sai.spring.fileservice.service.StorageService;
+import vla.sai.spring.fileservice.util.FileUtil;
 
 
 @RestController
-@RequestMapping (value ="/file_")
+@RequestMapping (value ="/files")
+@RequiredArgsConstructor
 public class FileController {
 
     private final StorageService storageService;
-    @Autowired
-    public FileUploadController(StorageService storageService) {
-        this.storageService = storageService;
+
+    @PostMapping("/upload-file")
+    public void uploadFile (@RequestParam("file") MultipartFile file) {
+        File uploadedFile = FileUtil.saveFile(file,"users");
     }
-    @GetMapping("/")
-    public String listUploadedFiles(Model model) throws IOException {
+//
+//    @GetMapping("/")
+//    public String listUploadedFiles(Model model) throws IOException {
+//
+//        model.addAttribute("files", storageService.loadAll().map(
+//                        path -> MvcUriComponentsBuilder.fromMethodName(FileController.class,
+//                                "serveFile", path.getFileName().toString()).build().toUri().toString())
+//                .collect(Collectors.toList()));
+//
+//        return "uploadForm";
+//    }
+//
+//    @GetMapping("/files/{filename:.+}")
+//    @ResponseBody
+//    public ResponseEntity<Resource> serveFile(@PathVariable String filename) {
+//
+//        Resource file = storageService.loadAsResource(filename);
+//
+//        if (file == null)
+//            return ResponseEntity.notFound().build();
+//
+//        return ResponseEntity.ok().header(HttpHeaders.CONTENT_DISPOSITION,
+//                "attachment; filename=\"" + file.getFilename() + "\"").body(file);
+//    }
+//    @PostMapping("/")
+//    public String handleFileUpload(@RequestParam("file") MultipartFile file,
+//                                   RedirectAttributes redirectAttributes) {
+//
+//        storageService.store(file);
+//        redirectAttributes.addFlashAttribute("message",
+//                "You successfully uploaded " + file.getOriginalFilename() + "!");
+//
+//        return "redirect:/";
+//    }
+//
+//    @GetMapping("/test_generator")
+//    public Map<String,String> testGenerator() {
+//        return Stream.of(FileInfo_.class.getDeclaredFields()) // достает атрибуты метамодели
+//                .filter(field -> Modifier.isFinal(field.getModifiers()) && Modifier.isStatic(field.getModifiers())) // фильтрация на отбор final/static атрибуты
+//                .collect(Collectors.toMap(
+//                        Field::getName,
+//                        field -> {
+//                            try { return (String) field.get(null);}
+//                            catch (IllegalAccessException e) { throw new RuntimeException(e);}
+//                        }
+//                        )
+//                );
+//    }
+//
 
-        model.addAttribute("files", storageService.loadAll().map(
-                        path -> MvcUriComponentsBuilder.fromMethodName(FileUploadController.class,
-                                "serveFile", path.getFileName().toString()).build().toUri().toString())
-                .collect(Collectors.toList()));
-
-        return "uploadForm";
-    }
-
-    @GetMapping("/files/{filename:.+}")
-    @ResponseBody
-    public ResponseEntity<Resource> serveFile(@PathVariable String filename) {
-
-        Resource file = storageService.loadAsResource(filename);
-
-        if (file == null)
-            return ResponseEntity.notFound().build();
-
-        return ResponseEntity.ok().header(HttpHeaders.CONTENT_DISPOSITION,
-                "attachment; filename=\"" + file.getFilename() + "\"").body(file);
-    }
-    @PostMapping("/")
-    public String handleFileUpload(@RequestParam("file") MultipartFile file,
-                                   RedirectAttributes redirectAttributes) {
-
-        storageService.store(file);
-        redirectAttributes.addFlashAttribute("message",
-                "You successfully uploaded " + file.getOriginalFilename() + "!");
-
-        return "redirect:/";
-    }
-
-
-
-
-
-
-
-
-
-
-
-    @GetMapping("/test_generator")
-    public Map<String,String> testGenerator() {
-        return Stream.of(FileInfo_.class.getDeclaredFields()) // достает атрибуты метамодели
-                .filter(field -> Modifier.isFinal(field.getModifiers()) && Modifier.isStatic(field.getModifiers())) // фильтрация на отбор final/static атрибуты
-                .collect(Collectors.toMap(
-                        Field::getName,
-                        field -> {
-                            try { return (String) field.get(null);}
-                            catch (IllegalAccessException e) { throw new RuntimeException(e);}
-                        }
-                        )
-                );
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
-    @ExceptionHandler(StorageFileNotFoundException.class)
-    public ResponseEntity<?> handleStorageFileNotFound(StorageFileNotFoundException exc) {
-        return ResponseEntity.notFound().build();
-    }
-
+//    @ExceptionHandler(StorageFileNotFoundException.class)
+//    public ResponseEntity<?> handleStorageFileNotFound(StorageFileNotFoundException exc) {
+//        return ResponseEntity.notFound().build();
+//    }
+//
 }
--- file-service/src/main/java/vla/sai/spring/fileservice/util/FileUtil.java
+++ file-service/src/main/java/vla/sai/spring/fileservice/util/FileUtil.java
@@ -1,2 +1,30 @@
-package vla.sai.spring.fileservice.util;public class FileUtil {
+package vla.sai.spring.fileservice.util;
+
+import org.apache.commons.io.FilenameUtils;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class FileUtil {
+    public static File saveFile(MultipartFile file, String login) {
+        try{
+            Path directoryPath = Files.createDirectory(Path.of("working_files",login));
+            String baseName = FilenameUtils.getBaseName(file.getOriginalFilename());
+            String extension = FilenameUtils.getExtension(file.getOriginalFilename());
+            String fileName = String.format("%s8%s", baseName, extension);
+            Path fullFilePath = Path.of(directoryPath.toString(), fileName);
+            file.transferTo(fullFilePath);
+            System.out.println("\n\n///////////////////////////// \n\n");
+            System.out.println("\n" + directoryPath.toString());
+            System.out.println("\n" + fileName + extension + baseName );
+            System.out.println("\n" + fullFilePath);
+            System.out.println("\n\n///////////////////////////// \n\n");
+            return fullFilePath.toFile();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
