73e94105fe419b6f9f9f30677b7fa317ae6f9387
VlaSai <ddddddddebat@gmail.com>
2025-04-17 16:48:37 +0300

    [1] ReportService init

---
+++ report-service/src/test/java/vla/sai/spring/reportservice/ReportServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package vla.sai.spring.reportservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReportServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
