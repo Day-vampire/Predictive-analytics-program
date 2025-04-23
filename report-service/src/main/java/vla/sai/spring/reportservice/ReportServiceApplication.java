2bbcdbdb25cb1667d0982525e01377a622612c8d
Day-vampire <102685336+Day-vampire@users.noreply.github.com>
2025-04-23 17:13:15 +0300

    [3] Report Service test git

--- report-service/src/main/java/vla/sai/spring/reportservice/ReportServiceApplication.java
+++ report-service/src/main/java/vla/sai/spring/reportservice/ReportServiceApplication.java
@@ -3,6 +3,7 @@ package vla.sai.spring.reportservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// hello
 @SpringBootApplication
 public class ReportServiceApplication {
 
