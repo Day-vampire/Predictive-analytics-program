511a822cca3d1cdbcebbd4349bfc59ea19d2186c
Day-vampire <102685336+Day-vampire@users.noreply.github.com>
2025-04-25 15:25:29 +0300

    [6] Report Service base PdfPart
    Добавлена базовая архитектура под пдф отчеты

---
+++ report-service/src/main/java/vla/sai/spring/reportservice/controller/PdfReportController.java
@@ -0,0 +1,54 @@
+package vla.sai.spring.reportservice.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody;
+import vla.sai.spring.reportservice.service.ExcelReportService;
+import vla.sai.spring.reportservice.service.PdfReportService;
+
+
+@RestController
+@RequestMapping(value = "/report/pdf")
+@RequiredArgsConstructor
+public class PdfReportController {
+
+    private final PdfReportService pdfReportService;
+
+    @PostMapping(path = "/data")
+    public ResponseEntity<StreamingResponseBody> exportDataToExcel(@RequestBody Object object) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=Data-report.pdf");
+        StreamingResponseBody stream = pdfReportService.dataToPdf(object);
+        return ResponseEntity
+                .ok()
+                .headers(headers)
+                .body(stream);
+    }
+
+    @PostMapping(path = "/smoothingGraph")
+    public ResponseEntity<StreamingResponseBody> exportSmoothingGraphToExcel(@RequestBody Object object) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=Smoothing-Graph-report.pdf");
+        StreamingResponseBody stream = pdfReportService.smoothingGraphToPdf(object);
+        return ResponseEntity
+                .ok()
+                .headers(headers)
+                .body(stream);
+    }
+
+    @PostMapping(path = "/holtWintersGraph")
+    public ResponseEntity<StreamingResponseBody> exportHoltWintersGraphToExcel(@RequestBody Object object) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=Holt-Winters-Graph-report.pdf");
+        StreamingResponseBody stream = pdfReportService.holtWintersGraphToPdf(object);
+        return ResponseEntity
+                .ok()
+                .headers(headers)
+                .body(stream);
+    }
+}
---
+++ report-service/src/main/java/vla/sai/spring/reportservice/service/PdfReportService.java
@@ -0,0 +1,10 @@
+package vla.sai.spring.reportservice.service;
+
+import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody;
+
+
+public interface PdfReportService {
+    StreamingResponseBody dataToPdf(Object object);
+    StreamingResponseBody smoothingGraphToPdf(Object object);
+    StreamingResponseBody holtWintersGraphToPdf(Object object);
+}
